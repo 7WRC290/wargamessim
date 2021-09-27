8c42d3d979027a5ee4b1004d3414dc8598383b8c
Daniel <daniel.januskevic@gmail.com>
2021-09-27 10:35:41 +0300

    Initial Commit

---
+++ wargamessim/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  wargamessim
+//
+//  Created by Daniel Januškevič on 2021-09-27.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
